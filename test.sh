299abbdbf44ecb9b82c55c1fc480c0ec94bbd394
国显达 <guoxianda@MacBook-Pro-3.local>
2024-06-24 16:15:21 +0800

    update test.sh

--- test.sh
+++ test.sh
@@ -1,32 +1,24 @@
-# # **************** For CASIA-B ****************
-# # Baseline
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 opengait/main.py --cfgs ./configs/baseline/baseline.yaml --phase test
+# # **************** For GREW ****************
+# SPOSGait-Small
+#CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7  python -u -m torch.distributed.launch --nproc_per_node=8  \\
+# opengait/main.py --cfgs configs/sposgait/retrain/sposgait_small_GREW-train500id_retrain.yaml --phase test
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7  python -u -m torch.distributed.launch --nproc_per_node=8  \\
+# opengait/main.py --cfgs configs/sposgait/retrain/sposgait_small_GREW-train5000id_retrain.yaml --phase test
+#CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7  python -u -m torch.distributed.launch --nproc_per_node=8  \\
+# opengait/main.py --cfgs configs/sposgait/retrain/sposgait_small_GREW-train20000id_retrain.yaml --phase test
 
-# # GaitSet
-# CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 opengait/main.py --cfgs ./configs/gaitset/gaitset.yaml --phase test
+# SPOSGait-Medium
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7  python -u -m torch.distributed.launch --nproc_per_node=8  \\
+# opengait/main.py --cfgs ./configs/sposgait/retrain/sposgait_medium_GREW-500id_retrain.yaml --phase test
+#CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7  python -u -m torch.distributed.launch --nproc_per_node=8  \\
+# opengait/main.py --cfgs ./configs/sposgait/retrain/sposgait_medium_GREW-5000id_retrain.yaml --phase test
+#CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -u -m torch.distributed.launch --nproc_per_node=8 \\
+# opengait/main.py --cfgs ./configs/sposgait/retrain/sposgait_medium_GREW-20000id_retrain.yaml --phase test
 
-# # GaitPart
-# CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 opengait/main.py --cfgs ./configs/gaitpart/gaitpart.yaml --phase test
-
-# GaitGL
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --master_port 12345 --nproc_per_node=4 opengait/main.py --cfgs ./configs/gaitgl/gaitgl.yaml --phase test
-
-# # GLN 
-# # Phase 1
-# CUDA_VISIBLE_DEVICES=3,4 python -m torch.distributed.launch --master_port 12345  --nproc_per_node=2 opengait/main.py --cfgs ./configs/gln/gln_phase1.yaml --phase test
-# # Phase 2
-# CUDA_VISIBLE_DEVICES=2,5 python -m torch.distributed.launch --nproc_per_node=2 opengait/main.py --cfgs ./configs/gln/gln_phase2.yaml --phase test
-
-
-# # **************** For OUMVLP ****************
-# # Baseline
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 opengait/main.py --cfgs ./configs/baseline/baseline_OUMVLP.yaml --phase test
-
-# # GaitSet
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 opengait/main.py --cfgs ./configs/gaitset/gaitset_OUMVLP.yaml --phase test
-
-# # GaitPart
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 opengait/main.py --cfgs ./configs/gaitpart/gaitpart_OUMVLP.yaml --phase test
-
-# GaitGL
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 opengait/main.py --cfgs ./configs/gaitgl/gaitgl_OUMVLP.yaml --phase test
+# SPOSGait-Large
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7  python -u -m torch.distributed.launch --nproc_per_node=8  \\
+# opengait/main.py --cfgs configs/sposgait/retrain/sposgait_large_GREW-train500id_retrain.yaml --phase test
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7  python -u -m torch.distributed.launch --nproc_per_node=8  \\
+# opengait/main.py --cfgs configs/sposgait/retrain/sposgait_large_GREW-train5000id_retrain.yaml --phase test
+  CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7  python -u -m torch.distributed.launch --nproc_per_node=8  \\
+ opengait/main.py --cfgs configs/sposgait/retrain/sposgait_large_GREW-train20000id_retrain.yaml --phase test
